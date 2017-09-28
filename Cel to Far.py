04a20b0be23aed005e78de2c2253ed287cee288c
allusernamesiwantedweretaken <31552430+allusernamesiwantedweretaken@users.noreply.github.com>
2017-09-27 19:26:41 -0500

    Add files via upload

---
+++ Cel to Far.py
@@ -0,0 +1,16 @@
+>>>
+>>> Celsius = float(input('Celsius: '))
+Celsius: 0
+>>> fahrenheit = (celsius * 9 / 5) + 32
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+NameError: name 'celsius' is not defined
+>>> ctemp = input('Enter the Celsius Temperature: ')
+Enter the Celsius Temperature: 23
+>>> ctempf = float(ctemp)
+>>> fahren = ctempf *9/5 + 32
+>>> pring = ('Converted Temperature is: ' , fahren, 'Fahrenheit')
+>>> print = ('Converted Temperature is: ' , fahren, 'Fahrenheit')
+>>> print
+('Converted Temperature is: ', 73.4, 'Fahrenheit')
+>>> 
